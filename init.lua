8a3425e12ecce278ea41658ed9757f01e3a33746
Jorge Rios <jorgeluisrios47@gmail.com>
2025-05-24 16:22:25 -0500

    setup LSP, treestitter, and some remaps

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", {
+    noremap = true,
+    silent = true
+})
+vim.wo.relativenumber = true
+vim.opt.wrap = false
+vim.o.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 8
+vim.opt.hlsearch = false
+vim.opt.termguicolors = true
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -12,36 +29,58 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-vim.wo.relativenumber = true
-vim.opt.wrap = false
-vim.o.clipboard = 'unnamedplus'
-
--- Setup lazy.nvim
-require("lazy").setup({
-    {
-        "mason-org/mason.nvim",
-        opts = {}
-    }, {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-                ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"}
+require("lazy").setup({{
+    "catppuccin/nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+        vim.cmd.colorscheme("catppuccin")
+    end
+}, {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {"lua", "python", "javascript", "html", "css"},
+            auto_install = true,
+            highlight = {
+                enable = true -- enable Treesitter-based highlighting
+            },
+            indent = {
+                enable = true -- enable Treesitter-based indentation
             }
-        end
-    }, {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        lazy = false,
-        config = function()
-            vim.o.background = "dark" -- or "light" for light mode
-            vim.cmd("colorscheme gruvbox")
-        end
-    }, {'HiPhish/rainbow-delimiters.nvim'}
-})
+        })
+    end
+}, {
+    "mason-org/mason.nvim",
+    config = function()
+        require("mason").setup()
+    end
+}, {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+        require("mason-lspconfig").setup({
+            ensure_installed = {"lua_ls"}
+        })
+    end
+}, {
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({})
+    end
+}, {
+    "nvimtools/none-ls.nvim",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {null_ls.builtins.formatting.stylua, null_ls.builtins.formatting.rubocop,
+                       null_ls.builtins.formatting.prettier, null_ls.builtins.diagnostics.rubocop,
+                       null_ls.builtins.diagnostics.eslint_d}
+        })
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end
+}, {'HiPhish/rainbow-delimiters.nvim'}})
